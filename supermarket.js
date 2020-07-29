569575f4514cf2dc55b0c4fc1421bb2733970383
jiaoguibin <jiaoguibin@zybank.com.cn>
2020-07-29 19:51:09 +0800

    [feat]需求二

--- supermarket.js
+++ supermarket.js
@@ -3,6 +3,8 @@ class SuperMarket {
         this.goodsMap = {
         }
 
+        this.promotionList = ['apple', 'watermelon']
+
         this.goodList = []
 
         this.init();
@@ -18,6 +20,35 @@ class SuperMarket {
         }
     }
 
+    // 增加促销商品 fixme：应该给相应用户权限开发该方法 @jiaoguibin
+    addPromotionGood (good) {
+        if (this.promotionList.indexOf(good) > -1) {
+            console.log('您添加的商品已经是促销商品')
+            return
+        }
+
+        if (!this.goodsMap[good]) {
+            console.log(`不好意思，该超市暂时没${good}，敬请期待吧`)
+            return
+        }
+
+        this.promotionList.push(good)
+    }
+
+    // 移出促销商品 fixme：应该给相应用户权限开发该方法 @jiaoguibin
+    removePromtionGood (good) {
+        if (this.promotionList.indexOf(good) === -1) {
+            console.log('该商品已经不是促销商品，请核实')
+            return
+        }
+
+        const index = this.promotionList.indexOf(good)
+        
+        // 移出
+        this.promotionList.splice(index, 1)
+    }
+
+    // 购买商品
     addGood (good) {
         if (!this.goodsMap[good]) {
             console.log(`不好意思，该超市暂时没${good}，敬请期待吧`)
@@ -27,10 +58,16 @@ class SuperMarket {
         this.goodList.push(good)
     }
     
+    // 计算积分
     calculateIntegral () {
         let integral = 0
         this.goodList.map(item => {
-            integral += this.goodsMap[item]
+            if (this.promotionList.indexOf(item) > -1) {
+                integral += this.goodsMap[item] * 2
+            } else {
+                integral += this.goodsMap[item]
+            }
+            
         })
         console.log(`您本次购买得到${integral}积分`)
         return integral
@@ -43,5 +80,6 @@ const superMarket = new SuperMarket()
 
 superMarket.addGood('apple')
 superMarket.addGood('watermelon')
+superMarket.addGood('washingLiquid')
 
 superMarket.calculateIntegral()
